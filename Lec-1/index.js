48f65cdf7f7f0f8f615a9feaef737cc4652f1add
kunjalsuratiya <dsutariya0007@gmail.com>
2024-12-16 10:37:24 +0530

    Node-Js

--- Lec-1/index.js
+++ Lec-1/index.js
@@ -1,1 +1,27 @@
 console.log("Hello world");
+
+
+const arr = [10, 4, 7, 30, 5]
+
+let ans = arr.filter((val) => {
+    return val % 2 == 1
+});
+console.log(ans);
+
+
+const arr2 = [10, 34, 17, 20, 5]
+
+let sum = arr.filter((val) => {
+    return val % 2 == 0
+});
+console.log(sum);
+
+
+
+let arr3 = [39,56,17,69,25]
+ arr3.forEach((val)=>{
+    console.log(val)
+ })
+
+
+ 
