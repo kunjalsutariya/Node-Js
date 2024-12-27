ca7e4f7a0bb9cbc572a135806dc80a3f2021ae17
kunjalsuratiya <dsutariya0007@gmail.com>
2024-12-27 19:31:43 +0530

    Todo-array

--- Lec-8-Todo-with-Array/index.js
+++ Lec-8-Todo-with-Array/index.js
@@ -1,35 +1,42 @@
 const express = require('express');
 
-const port = 9080;
+const port = 8000;
 const app = express();
 
-app.set('view engine' , 'ejs');
+app.set('view engine', 'ejs');
 
 app.use(express.urlencoded());
 
 let record = [];
 
-app.get('/' , (req , res) => {
-    return res.render('table',{
+app.get('/', (req, res) => {
+    return res.render('table', {
         record
     })
 })
-app.get('/add',(req,res)=>{
+app.get('/add', (req, res) => {
     return res.render('form')
 })
-app.post('/adduser',(req,res)=>{
-    const {userphone,username} = req.body;
+app.post('/adduser', (req, res) => {
+    const { userphone, username } = req.body;
     let obj = {
-        name:username,
-        phone:userphone
+        id:Math.floor(Math.random()*1000000),
+        name: username,
+        phone: userphone
     }
     record.push(obj);
     console.log('user Successfully register');
     return res.redirect('/');
-    
+
 })
-app.listen(port,(err)=>{
-    if(err){
+app.get('/deleteuser', (req, res) => {
+    let id = req.query.deleteId
+    let deletedata = record.filter(val => val.id != id);
+    record = deletedata;
+    return res.redirect('/');
+})
+app.listen(port, (err) => {
+    if (err) {
         console.log(err);
         return false
     }
